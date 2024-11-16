96a7972e67892be7d4f2ac82e8bf91c004dbe0fe
Chethan N V <chethan.nv3939@gmail.com>
2024-11-16 19:33:44 +0530

    main

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
+// MongoDB Connection
 mongoose.set("strictQuery", true);
 mongoose.connect(
   "mongodb+srv://chethannv:chethan@chethan.kjdlxwb.mongodb.net/CyberBullying"
@@ -17,9 +18,21 @@ db.on("error", () => {
   console.log("Database not Connected");
 });
 
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:3001", // React app origin
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
-app.use(cors());
+app.options("*", cors()); // Handle preflight requests
+
+// Routes
 app.use("/Signup-Login", SL);
+
+// Server
 const port = 5500;
 app.listen(port, () => {
   console.log("Server Started on " + port);
